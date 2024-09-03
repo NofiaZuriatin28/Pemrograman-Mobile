f6e7967fff7ee45000b1c6dcc9aaed18ef23ddc7
MC <nofiaisme28@gmail.com>
2024-09-03 19:30:16 +0700

    add Komentar.kt

---
+++ src/main/kotlin/Komentar.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Contoh komentar satu baris
+
+    /*
+    Contoh komentar multi baris
+     */
+
+    /**
+     * kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
