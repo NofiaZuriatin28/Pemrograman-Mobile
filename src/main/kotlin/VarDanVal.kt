f7fe6abd13e862cacd6eaeb519e645912955bccf
MC <nofiaisme28@gmail.com>
2024-09-03 20:01:23 +0700

    add VarDanVal.kt

---
+++ src/main/kotlin/VarDanVal.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    //var adalah variabel yang bisa diubah selama pengkodignan
+    var varVariabel: Int
+    varVariabel = 56
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+
+    /*
+    val adalah variabel yang nilainya tetap dan tidak bisa diubah
+    selama pengkodingan
+     */
+    val phi = 3.14
+
+}
