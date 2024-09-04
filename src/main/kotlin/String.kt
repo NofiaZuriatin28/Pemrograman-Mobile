865d7b8f2894fa9426b9f8a43a100c9d04db01a4
MC <nofiaisme28@gmail.com>
2024-09-04 18:39:07 +0700

    add String.kt

--- src/main/kotlin/String.kt
+++ src/main/kotlin/String.kt
@@ -4,4 +4,33 @@ fun main(args: Array<String>) {
         print(chr)
     }
     print('\n')
+
+
+    //string yang digabung dengan tipe data lain dengan mengguanakan operator +
+    println("")
+    println("===========================")
+    val s = "abc" + 1
+    println(s + "def")
+
+
+    //menyimpan data raw string
+    println("")
+    println("===========================")
+    var text = """
+        for (c in "foo)
+        print(c)
+        """
+    println(text)
+
+
+    // menghapus spasi yang tdk di perlukan pada raw string
+    println("")
+    println("===========================")
+    var texxt = """
+        |Tell me and i forget.
+        |Teach me and I remember
+        |Involve me and I learn.
+        |(Benjamin Franklin)
+        """.trimMargin()
+    println(texxt)
 }
