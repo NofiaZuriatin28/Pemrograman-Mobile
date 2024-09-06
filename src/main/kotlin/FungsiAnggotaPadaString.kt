907d7b91c1c5dd24e57008dfe5a9165c136a3644
MC <nofiaisme28@gmail.com>
2024-09-06 09:19:33 +0700

    add FungsiAnggotaPadaString.kt

--- src/main/kotlin/FungsiAnggotaPadaString.kt
+++ src/main/kotlin/FungsiAnggotaPadaString.kt
@@ -1,32 +1,27 @@
 fun main(args: Array<String>) {
-    //masih raguuuu!
     val s = "Rhinopotamus"
     println(s.startsWith("rhin"))
     println(s.endsWith("tamus"))
     println(s.contains("pot"))
     println(s.contains("lol"))
-
-    //toUpperCase() and tolowerCase()
+    //toUpperCase() and toLowerCase()
     var config = "Fullscreen shaDows autosave"
     config = config.toLowerCase()
-    println("Will the game run is fullscreen?")
-    println(config.contains("fullscreen?"))
+    println("Will the game run in fullscreen?")
+    println(config.contains("fullscreen"))
     println("Will shadows be turned on?")
     println(config.contains("shadows"))
     println("Will sound be turned off?")
     println(config.contains("nosound"))
     println("Would the player like to use autosave?")
     println(config.contains("autosave"))
-
-    //rplace(
+    //replace()
     var strJava = "Java is the best!"
-    strJava = strJava.replace("java", "kotlin")
+    strJava = strJava.replace("Java", "Kotlin")
     println(s)
     println("$strJava is ${strJava.length} characters long.")
-
     //substring()
-    println("I would not banish all of these Internets.".substring(2,7))
-
+    println("I would not banish all of these Internets.".substring(2, 7))
     //compareTo()
-    print("alpha".compareTo("bravo"))
+    println("alpha".compareTo("bravo"))
 }
